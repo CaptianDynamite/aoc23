1fbf7d7af34684607f35f404fea1de25874b7dea
CaptianDynamite <kalebvanm@gmail.com>
2023-12-01 16:38:29 +1000

    Part 2

--- day1.f90
+++ day1.f90
@@ -12,16 +12,58 @@ program aoc23
         tens = .true.
         find_first: do j = 1, len(input)
             current = input(j:j)
-            if ('0' <= current .and. current <= '9') then
+            temp = 0
+            if ('0' < current .and. current <= '9') then
                 read(current, '(I1)') temp
+            elseif (input(j:(j+2)) == 'one') then
+                temp = 1
+            elseif (input(j:(j+2)) == 'two') then
+                temp = 2
+            elseif (input(j:(j+4)) == 'three') then
+                temp = 3
+            elseif (input(j:(j+3)) == 'four') then
+                temp = 4
+            elseif (input(j:(j+3)) == 'five') then
+                temp = 5
+            elseif (input(j:(j+2)) == 'six') then
+                temp = 6
+            elseif (input(j:(j+4)) == 'seven')  then
+                temp = 7
+            elseif (input(j:(j+4)) == 'eight') then
+                temp = 8
+            elseif (input(j:(j+3)) == 'nine') then
+                temp = 9
+            end if
+            if (temp > 0) then
                 running_sum = running_sum + (temp * 10)
                 exit find_first
             end if
         end do find_first
         find_last: do j = len(input), 1, -1
             current = input(j:j)
-            if ('0' <= current .and. current <= '9') then
+            temp = 0
+            if ('0' < current .and. current <= '9') then
                 read(current, '(I1)') temp
+            elseif (input(j:(j+2)) == 'one') then
+                temp = 1
+            elseif (input(j:(j+2)) == 'two') then
+                temp = 2
+            elseif (input(j:(j+4)) == 'three') then
+                temp = 3
+            elseif (input(j:(j+3)) == 'four') then
+                temp = 4
+            elseif (input(j:(j+3)) == 'five') then
+                temp = 5
+            elseif (input(j:(j+2)) == 'six') then
+                temp = 6
+            elseif (input(j:(j+4)) == 'seven')  then
+                temp = 7
+            elseif (input(j:(j+4)) == 'eight') then
+                temp = 8
+            elseif (input(j:(j+3)) == 'nine') then
+                temp = 9
+            end if
+            if (temp > 0) then
                 running_sum = running_sum + temp
                 exit find_last
             end if
