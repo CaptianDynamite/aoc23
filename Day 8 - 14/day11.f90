7ecbaa5cd49b21c5ab5eeb244901873a141f609a
CaptianDynamite <kalebvanm@gmail.com>
2023-12-11 15:39:12 +1000

    Day 11 part 2

--- Day 8 - 14/day11.f90
+++ Day 8 - 14/day11.f90
@@ -25,13 +25,16 @@ contains
 end module day11helper
 
 program day11
+    use iso_fortran_env, only: int64
     use day11helper
     implicit none
     character(len=512) :: cur_line
     logical, allocatable :: column_empty(:), row_empty(:)
     type(position), allocatable :: galaxies(:)
-    integer :: iostat, i, j, lines, cur_x, cur_y, gal_count, cur_gal
+    type(position), allocatable :: galaxies_pt2(:)
+    integer :: iostat, i, j, lines, cur_x, cur_y, cx2, cy2, gal_count, cur_gal
     integer :: part1
+    integer(int64) :: part2
 
     open(21, file='day11.txt', status='old')
     iostat = 0
@@ -56,37 +59,48 @@ program day11
     end do
     rewind(21)
     allocate(galaxies(gal_count))
+    allocate(galaxies_pt2(gal_count))
     cur_y = 0
+    cy2 = 0
     cur_gal = 0
     do i = 1, lines
         read(21, '(A)', iostat=iostat) cur_line
         cur_x = 0
+        cx2 = 0
         cur_y = cur_y + 1
+        cy2 = cy2 + 1
         if (row_empty(i)) then
             cur_y = cur_y + 1
+            cy2 = cy2 + 999999
             cycle
         end if
         do j = 1, len(trim(cur_line))
             cur_x = cur_x + 1
+            cx2 = cx2 + 1
             if (column_empty(j)) then
                 cur_x = cur_x + 1
+                cx2 = cx2 + 999999
                 cycle
             end if
             if (cur_line(j:j) == '#') then
                 cur_gal = cur_gal + 1
                 galaxies(cur_gal) = position(cur_x, cur_y)
+                galaxies_pt2(cur_gal) = position(cx2, cy2)
             end if
         end do
     end do
     close(21)
 
     part1 = 0
+    part2 = 0
     do i = 1, gal_count
         do j = i + 1, gal_count
             part1 = part1 + galaxies(i)%taxicab_dist(galaxies(j))
+            part2 = part2 + galaxies_pt2(i)%taxicab_dist(galaxies_pt2(j))
         end do
     end do
 
     print *, "Part 1: ", part1
+    print *, "Part 2: ", part2
 
 end program day11
