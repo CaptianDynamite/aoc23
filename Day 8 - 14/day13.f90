e62ba2f6bf7dad259d5cfdf61a84afdef4495ffe
CaptianDynamite <kalebvanm@gmail.com>
2023-12-13 17:44:07 +1000

    Day 13 part 2 & timing code

--- Day 8 - 14/day13.f90
+++ Day 8 - 14/day13.f90
@@ -1,27 +1,32 @@
 module day13helper
     implicit none
 contains
-    impure function row_reflect_loc(map) result(loc)
+    impure function row_reflect_loc(map, smudges) result(loc)
         character, intent(in) :: map(:, :)
+        integer, intent(in) :: smudges
         integer :: loc
-        integer :: fwd, back, i, m, mi
+        integer :: fwd, back, i, m, mi, smudge_remain, diff
         integer :: reflect_length(size(map, 2))
         logical :: reflect
         loc = 0
         reflect_length = 0
         do i = 1, size(map, 2) - 1
+            smudge_remain = smudges
             fwd = 1
             back = 0
             reflect = .true.
             inner: do while (i + back > 0 .and. i + fwd <= size(map, 2))
-                if (any(map(:, i + back) /= map(:, i + fwd))) then
+                diff = count(map(:, i + back) /= map(:, i + fwd))
+                if (diff > smudge_remain) then
                     reflect = .false.
                     exit inner
+                else
+                    smudge_remain = smudge_remain - diff
                 end if
                 fwd = fwd + 1
                 back = back - 1
             end do inner
-            if (reflect) then
+            if (reflect .and. smudge_remain == 0) then
                 reflect_length(i) = fwd
             end if
         end do
@@ -38,6 +43,7 @@ contains
 end module day13helper
 
 program day13
+    use iso_fortran_env, only: int64
     use day13helper
     implicit none
 
@@ -45,10 +51,13 @@ program day13
     character(len=512) :: cur_map(512) ! Arbitrary but large enough
     character, allocatable :: map(:, :), tmap(:, :)
     integer :: iostat, lines, line_length, i, j, fwd, back, reflect, temp
-    integer :: part1
+    integer :: part1, part2
+    integer(int64) :: begin, end, rate
 
+    call system_clock(begin, rate)
     open(24, file='day13.txt', status='old')
     part1 = 0
+    part2 = 0
     lines = 0
     line_length = 0
     do while (.true.)
@@ -66,12 +75,19 @@ program day13
                 end do
             end do
 
-            temp = row_reflect_loc(map)
-            part1 = part1 + 100 * temp
             allocate(tmap(lines, line_length))
+            tmap = transpose(map)
+
+            temp = row_reflect_loc(map, 0)
+            part1 = part1 + 100 * temp
             if (temp == 0) then
-                tmap = transpose(map)
-                part1 = part1 + row_reflect_loc(tmap)
+                part1 = part1 + row_reflect_loc(tmap, 0)
+            end if
+
+            temp = row_reflect_loc(map, 1)
+            part2 = part2 + 100 * temp
+            if (temp == 0) then
+                part2 = part2 + row_reflect_loc(tmap, 1)
             end if
 
             line_length = 0
@@ -89,5 +105,8 @@ program day13
     close(24)
 
     print *, "Part 1: ", part1
+    print *, "Part 2: ", part2
+    call system_clock(end)
+    print *, "Day 13 took: ", end - begin, " cycles, where there are ", rate, " cycles/s"
 
 end program day13
