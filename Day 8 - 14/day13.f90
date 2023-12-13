f0aa152dd37429c7c4a8bd7bce4759fa1e5b1e55
CaptianDynamite <kalebvanm@gmail.com>
2023-12-13 17:28:19 +1000

    Day 13 part 1

--- Day 8 - 14/day13.f90
+++ Day 8 - 14/day13.f90
@@ -1,10 +1,40 @@
 module day13helper
     implicit none
 contains
-    subroutine test(string)
-        character(len=*), intent(in) :: string
-        print *, string
-    end subroutine test
+    impure function row_reflect_loc(map) result(loc)
+        character, intent(in) :: map(:, :)
+        integer :: loc
+        integer :: fwd, back, i, m, mi
+        integer :: reflect_length(size(map, 2))
+        logical :: reflect
+        loc = 0
+        reflect_length = 0
+        do i = 1, size(map, 2) - 1
+            fwd = 1
+            back = 0
+            reflect = .true.
+            inner: do while (i + back > 0 .and. i + fwd <= size(map, 2))
+                if (any(map(:, i + back) /= map(:, i + fwd))) then
+                    reflect = .false.
+                    exit inner
+                end if
+                fwd = fwd + 1
+                back = back - 1
+            end do inner
+            if (reflect) then
+                reflect_length(i) = fwd
+            end if
+        end do
+        m = 1
+        mi = 0
+        do i = 1, size(reflect_length)
+            if (reflect_length(i) >= m) then
+                m = reflect_length(i)
+                mi = i
+            end if
+        end do
+        loc = mi
+    end function row_reflect_loc
 end module day13helper
 
 program day13
@@ -12,16 +42,52 @@ program day13
     implicit none
 
     character(len=512) :: cur_line ! Arbitrary but large enough
-    integer :: iostat
+    character(len=512) :: cur_map(512) ! Arbitrary but large enough
+    character, allocatable :: map(:, :), tmap(:, :)
+    integer :: iostat, lines, line_length, i, j, fwd, back, reflect, temp
+    integer :: part1
 
     open(24, file='day13.txt', status='old')
+    part1 = 0
+    lines = 0
+    line_length = 0
     do while (.true.)
         read(24, '(A)', iostat=iostat) cur_line
-        if (iostat /= 0) then
-            exit
+        if (line_length == 0) then
+            line_length = len(trim(cur_line))
+            cur_map = ''
         end if
-        call test(trim(cur_line))
+        if (iostat /= 0 .or. cur_line == '') then
+            allocate(map(line_length, lines))
+            do i = 1, lines
+                cur_line = cur_map(i)
+                do j = 1, line_length
+                    map(j, i) = cur_line(j:j)
+                end do
+            end do
+
+            temp = row_reflect_loc(map)
+            part1 = part1 + 100 * temp
+            allocate(tmap(lines, line_length))
+            if (temp == 0) then
+                tmap = transpose(map)
+                part1 = part1 + row_reflect_loc(tmap)
+            end if
+
+            line_length = 0
+            lines = 0
+            if (iostat /= 0) then
+                exit
+            end if
+            deallocate(tmap)
+            deallocate(map)
+        else
+            lines = lines + 1
+            cur_map(lines) = cur_line
+        end if
     end do
     close(24)
+
+    print *, "Part 1: ", part1
 
 end program day13
