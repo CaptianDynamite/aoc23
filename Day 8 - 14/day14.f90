f193faf3554562343b5ded384a7f54981c0e9101
CaptianDynamite <kalebvanm@gmail.com>
2023-12-14 16:14:22 +1000

    Day 14 preparation

---
+++ Day 8 - 14/day14.f90
@@ -0,0 +1,13 @@
+module day14helper
+    use iso_fortran_env, only: int64
+    implicit none
+contains
+
+end module day14helper
+
+program day14
+    use iso_fortran_env, only: int64
+    use day14helper
+    implicit none
+    print *, 'Day 14!'
+end program day14
